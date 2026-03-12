f9919bd823308b2ecd2b2b2c658259f86f932abb
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-03-12 09:40:29 +0100

    Remove outdated incorrect comment
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- asm330lhhx_reg.h
+++ asm330lhhx_reg.h
@@ -1669,7 +1669,7 @@ float_t asm330lhhx_from_lsb_to_nsec(int32_t lsb);
 typedef enum
 {
   ASM330LHHX_2g   = 0,
-  ASM330LHHX_16g  = 1, /* if XL_FS_MODE = '1' -> ASM330LHHX_2g */
+  ASM330LHHX_16g  = 1,
   ASM330LHHX_4g   = 2,
   ASM330LHHX_8g   = 3,
 } asm330lhhx_fs_xl_t;
